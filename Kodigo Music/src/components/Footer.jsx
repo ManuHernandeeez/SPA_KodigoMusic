cf674071a743772a4baa161d2a692e4c0ae2ea79
Manuel Hernandez <manu.hernandez.15.2002@gmail.com>
2025-07-01 23:10:07 -0600

    Ajustes footer

--- Kodigo Music/src/components/Footer.jsx
+++ Kodigo Music/src/components/Footer.jsx
@@ -3,11 +3,37 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-content">
+      <div className="footer-columns">
+        <div className="footer-col">
+          <h4>Compañía</h4>
+          <ul>
+            <li>Acerca de</li>
+            <li>Empleos</li>
+            <li>Prensa</li>
+            <li>Blog</li>
+          </ul>
+        </div>
+        <div className="footer-col">
+          <h4>Ayuda</h4>
+          <ul>
+            <li>Soporte</li>
+            <li>Centro de ayuda</li>
+            <li>Contacto</li>
+            <li>Reportar un problema</li>
+          </ul>
+        </div>
+        <div className="footer-col">
+          <h4>Legal</h4>
+          <ul>
+            <li>Términos de uso</li>
+            <li>Privacidad</li>
+            <li>Cookies</li>
+            <li>Licencias</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-bottom">
         <span>Kodigo Music &copy; {new Date().getFullYear()}</span>
-        <span>
-          Hecho con <span style={{color: "#646cff"}}>a</span> por tu equipo Kodigo
-        </span>
       </div>
     </footer>
   );
